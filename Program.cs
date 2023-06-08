ab24855f8aa23d76032d7375565aefa05c3b720d
GitTurtleDone <giang2110@gmail.com>
2023-06-08 16:04:58 +1200

    upload to one excel Worksheet

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ class Program
 {
     static void Main()
     {
+        /*
+        
         string csvFilePath = @"/mnt/c/Users/Dell User/" + 
                             @"OneDrive - University of Canterbury/" + 
                             @"NZ2208/NghienCuu/SemSem/Projects/betaGa2O3MESFETs/230512_Fab230504to0607/230519_Fab230509/Dev02/A01/" +
@@ -28,17 +30,30 @@ class Program
                                @"OneDrive - University of Canterbury/" + 
                                @"NZ2208/NghienCuu/SemSem/Projects/betaGa2O3MESFETs/230512_Fab230504to0607/230519_Fab230509/Dev02/" +
                                 @"IrOx230509_02_A01.xlsx";
-        Console.WriteLine(csvFilePath);
-        Console.WriteLine(excelFilePath);
+        
+        */
+        string excelFilePath = @"IrOx230509_02_A01.xlsx";
+        string csvFilePath = @"I_V Diode Full wo PowComp.csv";
+        
+        uploadToOneSheet (csvFilePath, excelFilePath, "Full", 7, 1);
+    }
+    static void uploadToOneSheet (string fCsvFilePath, string fExcelFilePath, string fSheetName, int fStartRow, int fStartCol)
+    {
+        
+        
+        
+        // Console.WriteLine(csvFilePath);
+        // Console.WriteLine(excelFilePath);
         try
         {
             // Read the CSV file
-            using (StreamReader reader = new StreamReader(csvFilePath))
+            using (StreamReader reader = new StreamReader(fCsvFilePath))
             {
                 try
                 {
-                    using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFilePath)))
+                    using (ExcelPackage package = new ExcelPackage(new FileInfo(fExcelFilePath)))
                     {
+                        /*
                         ExcelWorkbook workbook = package.Workbook;
                         if (workbook == null)
                         {
@@ -47,21 +62,23 @@ class Program
                         }
                         else 
                         {
-                             Console.WriteLine("I have gone in here");
-                             foreach (ExcelWorksheet ws in workbook.Worksheets)
+                            Console.WriteLine("I have gone in here");
+                            var worksheets = package.Workbook.Worksheets.Select(x => x.Name);
+                            //foreach (var sheet in worksheets) Console.WriteLine(sheet);
+                            foreach (var ws in worksheets)
                                 {
                                     Console.WriteLine("hellow");
-                                    Console.WriteLine(ws.Name);
+                                    Console.WriteLine(ws);
                                 }
                         }
-                        ExcelWorksheet worksheet = package.Workbook.Worksheets["Full"];
+                        */
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets[fSheetName];
                         if (worksheet == null)
                         {
                             Console.WriteLine("Worksheet not found!");
                             return;
                         }
-                        
-                        int row = 7;
+                        int row = fStartRow;
                         while (!reader.EndOfStream)
                         {
                             string line = reader.ReadLine();
@@ -69,14 +86,14 @@ class Program
 
                             for (int col = 0; col < data.Length; col++)
                             {
-                                worksheet.Cells[row, col + 1].Value = data[col];
+                                worksheet.Cells[row, col + fStartCol].Value = data[col];
                             }
 
                             row++;
                         }
 
                         // Save the Excel file
-                        FileInfo excelFile = new FileInfo(excelFilePath);
+                        FileInfo excelFile = new FileInfo(fExcelFilePath);
                         package.SaveAs(excelFile);
                     }
                 }
@@ -100,6 +117,7 @@ class Program
         {
             Console.WriteLine("An error occurred while reading or writing the csv file!");
         }
+    
     }
 }
 
