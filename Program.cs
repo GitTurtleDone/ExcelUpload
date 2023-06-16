f898a161522006cca627c822ed9245f74b210f8b
GitTurtleDone <giang2110@gmail.com>
2023-06-16 15:44:18 +1200

    DeviceFolder Implementation

--- Program.cs
+++ Program.cs
@@ -23,118 +23,179 @@ class Program
 {
     static void Main()
     {
-        /*
         
-        string csvFilePath = @"I_V Diode Full wo PowComp.csv";
-        string excelFilePath = @"/mnt/c/Users/Dell User/" +
-                               @"OneDrive - University of Canterbury/" + 
-                               @"NZ2208/NghienCuu/SemSem/Projects/betaGa2O3MESFETs/230512_Fab230504to0607/230519_Fab230509/Dev02/" +
-                                @"IrOx230509_02_A01.xlsx";
-        
-        */
-        string excelFilePath = @"IrOx230509_02_A01.xlsx";
-        string csvFilePath = @"../230512_Fab230504to0607/230519_Fab230509/Dev02/A01/" +
+        string csvFilePath = @"../230512_Fab230504to0607/230606_Fab230509IrOxRecess/Dev02/A01/" +
                             @"I_V Diode Full wo PowComp [02 A01(2) ; 19_05_2023 1_41_45 p.m.].csv";
-        
-        
-        
-        //uploadToOneSheet (csvFilePath, excelFilePath, "Full", 7, 1);
         MeasFile measFile = new MeasFile();
         SBDFolder SBDFolder = new SBDFolder();
-        
+        DeviceFolder devFolder = new DeviceFolder();
+        /*
         measFile.CsvFilePath = csvFilePath;
         measFile.analyzeFile();
         SBDFolder.FolderPath = measFile.CsvFolderPath;
-        SBDFolder.UploadDetailTemplatePath = "../UploadDetailsTemplate.csv";
-        SBDFolder.getAllCsvFileNames();
-        SBDFolder.getAllMeasTimes();
-        SBDFolder.getAllMeasTypes();
-        SBDFolder.getAllIsLasts();
-        SBDFolder.getSBDID();
-        SBDFolder.getSBDType();
-        SBDFolder.getSamplID();
-        SBDFolder.createWorkbook();
-        SBDFolder.getUploadDetailTemplate();
-        SBDFolder.Upload();        // DateTime currentDateTime = DateTime.Now;
-        // string formattedDateTime = currentDateTime.ToString();  // Format based on system settings
-        // Console.WriteLine(formattedDateTime);
-    
-        
-        //Console.WriteLine($"Here is the folder path: {measFile.CsvFolderPath} ");
-        
+        SBDFolder.processSBDFolder();        
+        */
+        devFolder.FolderPath = @"../230512_Fab230504to0607/230606_Fab230509IrOxRecess/Dev02/";
+        devFolder.getAllSubFolderPaths();
+        devFolder.getSampleID();
+        devFolder.getAllSubFolderNames();
+        devFolder.getAllSBDFolders();
+        devFolder.getAllRev500Dones();
+        devFolder.getLogFilePath();
+        devFolder.getMemoFilePath();
+        devFolder.memoWrite();
+        devFolder.processAllSBDFolders();        
     }
-    
-    /*
-    public static Person GetPersonDetails()
-    {
-        int age = 30;
-        string name = "John Doe";
-        return new Person { Age = age, Name = name };
-    }
-
-    // Usage
-    Person person = GetPersonDetails();
-    int age = person.Age;
-    string name = person.Name;
-    */
 
 }
-public class MeasFile
+public class DeviceFolder
+
 {
-    public string CsvFilePath { get; set; }
-    public string CsvFolderPath{get; set;}
-    public string CsvFileName{get; set;}
-    public string MeasType { get; set; } // Type of measuremnt
-    public void analyzeFile() // get the file name and the folder path from the file path
+    public string FolderPath { get; set;}
+    
+    public string AllSBDIDs { get; set;}
+    public string AllSBDTypes { get; set;}
+    public string SampleID { get; set;}
+    public string LogFilePath {get; set;}
+    public string LogMessage {get; set;}
+    public string MemoFilePath {get;set;} 
+    public string Memo {get; set;}
+    public List<SBDFolder> AllSBDFolders { get; set;} = new List<SBDFolder>();
+    public List<string> AllSubFolderPaths { get; set;} = new List<string>();
+    public List<string> AllSubFolderNames { get; set;} = new List<string>();
+    public List<bool> AllRev500Dones { get; set;} = new List<bool>();
+    
+    public void getAllSubFolderPaths()
     {
-        
-        if (!string.IsNullOrEmpty(CsvFilePath))
+        if (!string.IsNullOrEmpty(FolderPath))
         {
-            if (string.IsNullOrEmpty(CsvFileName)) CsvFileName = Path.GetFileName(CsvFilePath);
-            if (string.IsNullOrEmpty(CsvFolderPath)) CsvFolderPath = Path.GetDirectoryName(CsvFilePath);
-            Console.WriteLine(CsvFileName);
-            Console.WriteLine(CsvFolderPath);
+            string[] allPaths = Directory.GetDirectories(FolderPath, "*", SearchOption.AllDirectories);
+            foreach (string subfolder in allPaths)
+            {
+                AllSubFolderPaths.Add(subfolder);
+                Console.WriteLine($"Subfolder path: {subfolder}");
+            }
+        }
+    }
+    public void getSampleID()
+    {
+        if (!string.IsNullOrEmpty(FolderPath))
+        {
+            SampleID = FolderPath.Substring(FolderPath.LastIndexOf("_")+1, FolderPath.LastIndexOf("/")-FolderPath.LastIndexOf("_")-1).Replace("/","_");
+            Console.WriteLine($"Sample ID: {SampleID}");
+        }
+    }
+    public void getAllSubFolderNames()
+    {
+        if (AllSubFolderPaths.Count == 0)
+        {
+            Console.WriteLine("AllSubFolderPaths list is empty");
+        } 
+        else
+        {
+            for (int i = 0; i < AllSubFolderPaths.Count; i++)
+            {
+                Console.WriteLine($"Folder Name: {AllSubFolderPaths[i].Substring(AllSubFolderPaths[i].Length-3)}");
+                AllSubFolderNames.Add(AllSubFolderPaths[i].Substring(AllSubFolderPaths[i].Length-3));
+            }
+        }
+    }
+
+    public void getAllSBDFolders()
+    {
+        if (AllSubFolderPaths.Count == 0)
+        {
+            Console.WriteLine("AllSubFolderPaths list is empty");
+        } 
+        else
+        {
+            for (int i = 0; i < AllSubFolderPaths.Count; i++)
+            {
+                SBDFolder sbdFolder = new SBDFolder();
+                sbdFolder.FolderPath = AllSubFolderPaths[i];
+                AllSBDFolders.Add(sbdFolder);
+                Console.WriteLine($"Folder Name: {sbdFolder.FolderPath.Substring(AllSubFolderPaths[i].Length-3)}");
+            }
+        }
+    }
+    public void processAllSBDFolders()
+    {
+        if (AllSBDFolders.Count == 0)
+        {
+            Console.WriteLine("AllSBDFolders list is empty");
         }
         else
         {
-            Console.WriteLine("File path is empty or null.");
+            for (int i = 0; i < AllSBDFolders.Count; i++)
+            {
+                AllSBDFolders[i].processSBDFolder();
+                LogMessage = $"Folder {AllSubFolderNames[i]} has been processed";
+                logWrite();
+            }
         }
-            
-    } 
-    public void getMeasurementType() // get the measurement type fromt the file name
-    {
-        int index = CsvFileName.IndexOf(@"[");
-        if (index != -1)
-        {
-            MeasType = CsvFileName.Substring(0, index-1);
-            Console.WriteLine(MeasType);
-        }
-        else if (CsvFileName.Contains("Stop3"))
-        {
-            MeasType = "KL For";
-        }
-        else if (CsvFileName.Contains("StopM3"))
-        {
-            MeasType = "KL Rev";
-        }
-        else if (CsvFileName.Contains("StopM500"))
-        {
-            MeasType = "Pico Rev 500";
-        }
-        else MeasType = "No Type";
-
     }
-    
-}
+    public void getLogFilePath()
+    {
+        LogFilePath = Path.Combine(FolderPath, "log.txt");
+    }
+
+    public void logWrite()
+    {
+        if (!string.IsNullOrEmpty(LogFilePath)) File.AppendAllText(LogFilePath, $"{DateTime.Now}: {LogMessage}\n");
+    }
+    public void getAllRev500Dones()
+    {
+        if (AllSBDFolders.Count == 0)
+        {
+            Console.WriteLine("AllSBDFolders list is empty");
+        }
+        else
+        {
+            for (int i = 0; i < AllSBDFolders.Count; i++)
+            {
+                AllSBDFolders[i].getRev500Done();
+                AllRev500Dones.Add(AllSBDFolders[i].Rev500Done);
+            }
+        }
+    }
+
+    public void getMemoFilePath()
+    {
+        MemoFilePath = Path.Combine(FolderPath, "memo.txt");
+    }
+
+    public void memoWrite()
+    {
+        if (AllRev500Dones.Count == 0) 
+        {
+            Console.WriteLine("AllRev500Dones list is empty");
+        }
+        else
+        {
+            for (int i = 0; i < AllRev500Dones.Count; i++)
+            {
+                Memo = $"{AllSubFolderNames[i]},{AllRev500Dones[i]}";
+                File.AppendAllText(MemoFilePath, $"{Memo}\n");
+                Console.WriteLine(Memo);
+            }
+            
+        }
+    }
+
+}    
+
 public class SBDFolder
 {
     public string FolderPath { get; set;}
     public string WorkbookPath { get; set; }
     public string UploadDetailTemplatePath{ get; set;}
+    public string LogFilePath {get; set;}
+    public string LogMessage {get; set;}
     public string SampleID { get; set;}
     public string SBDID { get; set;}
     public string SBDType { get; set;}
     public string[] AllFilePaths { get; set;}
+    public bool Rev500Done {get; set;}
     public List<string> AllCsvFileNames { get; set;} = new List<string>();
     public List<string> AllCsvFilePaths { get; set;} = new List<string>();
     public List<string> AllMeasTypes { get; set;} = new List<string>();
@@ -144,7 +205,32 @@ public class SBDFolder
     public List<(string, string, int, int)> UploadDetailTemplates { get; set;} = new List<(string, string, int, int)>();
     //public ExcelPackage Workbook;
     
-    
+    public void processSBDFolder()
+    {
+        if (!string.IsNullOrEmpty(FolderPath))
+        {
+            UploadDetailTemplatePath = "../UploadDetailsTemplate.csv";
+            getAllCsvFileNames();
+            getAllMeasTimes();
+            getAllMeasTypes();
+            getAllIsLasts();
+            getSBDID();
+            getSBDType();
+            getSamplID();
+            createWorkbook();
+            getUploadDetailTemplate();
+            getLogFilePath();
+            getRev500Done();
+            Upload();
+        }  
+    }
+    public void getRev500Done()
+    {
+        if (AllMeasTypes.Contains("Pico Rev 500")) 
+            Rev500Done = true; 
+        else 
+            Rev500Done = false;
+    }
     public void getSamplID()
     {
         SampleID = FolderPath.Substring(FolderPath.LastIndexOf("_")+1, FolderPath.LastIndexOf("/")-FolderPath.LastIndexOf("_")-1).Replace("/","_");
@@ -159,6 +245,15 @@ public class SBDFolder
     {
         SBDType = FolderPath.Substring(FolderPath.Length-3,1);
         Console.WriteLine(SBDType);
+    }
+    public void getLogFilePath()
+    {
+        LogFilePath = Path.Combine(FolderPath, "log.txt");
+    }
+
+    public void logWrite()
+    {
+        if (!string.IsNullOrEmpty(LogFilePath)) File.AppendAllText(LogFilePath, $"{DateTime.Now}: {LogMessage}\n");
     }
     public void getAllCsvFileNames()
     {
@@ -367,10 +462,11 @@ public class SBDFolder
                         if (AllMeasTypes[i] == UploadDetailTemplates[j].Item1)
                         {
                             AllUploadDetails.Add(UploadDetailTemplates[j]);
-                            Console.WriteLine($"data to upload: {AllCsvFileNames[i]},sheet: {UploadDetailTemplates[j].Item2}");
+                            Console.WriteLine($"data to upload: {AllCsvFileNames[i]}, sheet: {UploadDetailTemplates[j].Item2}");
                             uploadToOneSheet(WorkbookPath,Path.Combine(FolderPath,AllCsvFileNames[i]),UploadDetailTemplates[j].Item2,
                                             UploadDetailTemplates[j].Item3,
                                             UploadDetailTemplates[j].Item4);
+                            logWrite();
                         }
                     }
                 }
@@ -428,10 +524,6 @@ public class SBDFolder
 }
     public void uploadToOneSheet (string fExcelFilePath, string fCsvFilePath, string fSheetName, int fStartRow, int fStartCol)
     {
-        // Console.WriteLine(csvFilePath);
-        // Console.WriteLine(excelFilePath);
-        // fCsvFilePath = $"\"{fCsvFilePath}\"";
-        // fExcelFilePath = $"\"{fExcelFilePath}\"";
         try
         {
             // Read the CSV file
@@ -441,25 +533,6 @@ public class SBDFolder
                 {
                     using (ExcelPackage package = new ExcelPackage(new FileInfo(fExcelFilePath)))
                     {
-                        /*
-                        ExcelWorkbook workbook = package.Workbook;
-                        if (workbook == null)
-                        {
-                            Console.WriteLine("Workbook not found");
-                            return;
-                        }
-                        else 
-                        {
-                            Console.WriteLine("I have gone in here");
-                            var worksheets = package.Workbook.Worksheets.Select(x => x.Name);
-                            //foreach (var sheet in worksheets) Console.WriteLine(sheet);
-                            foreach (var ws in worksheets)
-                                {
-                                    Console.WriteLine("hellow");
-                                    Console.WriteLine(ws);
-                                }
-                        }
-                        */
                         package.Workbook.CalcMode = ExcelCalcMode.Manual;
                         ExcelWorksheet worksheet = package.Workbook.Worksheets[fSheetName];
                         if (worksheet == null)
@@ -492,6 +565,7 @@ public class SBDFolder
 
                         // Save the Excel file
                         //FileInfo excelFile = new FileInfo(fExcelFilePath);
+                        LogMessage = $"Data file: {Path.GetFileName(fCsvFilePath)} has been successfully imported to the sheet: {fSheetName} in {Path.GetFileName(fExcelFilePath)}";
                         package.Workbook.CalcMode = ExcelCalcMode.Automatic;
                         package.Save();
                     }
@@ -521,6 +595,54 @@ public class SBDFolder
     
 }
 
+
+public class MeasFile
+{
+    public string CsvFilePath { get; set; }
+    public string CsvFolderPath{get; set;}
+    public string CsvFileName{get; set;}
+    public string MeasType { get; set; } // Type of measuremnt
+    public void analyzeFile() // get the file name and the folder path from the file path
+    {
+        
+        if (!string.IsNullOrEmpty(CsvFilePath))
+        {
+            if (string.IsNullOrEmpty(CsvFileName)) CsvFileName = Path.GetFileName(CsvFilePath);
+            if (string.IsNullOrEmpty(CsvFolderPath)) CsvFolderPath = Path.GetDirectoryName(CsvFilePath);
+            Console.WriteLine(CsvFileName);
+            Console.WriteLine(CsvFolderPath);
+        }
+        else
+        {
+            Console.WriteLine("File path is empty or null.");
+        }
+            
+    } 
+    public void getMeasurementType() // get the measurement type fromt the file name
+    {
+        int index = CsvFileName.IndexOf(@"[");
+        if (index != -1)
+        {
+            MeasType = CsvFileName.Substring(0, index-1);
+            Console.WriteLine(MeasType);
+        }
+        else if (CsvFileName.Contains("Stop3"))
+        {
+            MeasType = "KL For";
+        }
+        else if (CsvFileName.Contains("StopM3"))
+        {
+            MeasType = "KL Rev";
+        }
+        else if (CsvFileName.Contains("StopM500"))
+        {
+            MeasType = "Pico Rev 500";
+        }
+        else MeasType = "No Type";
+
+    }
+    
+}
 
 
 
